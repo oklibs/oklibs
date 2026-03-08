abe9c96950419dac7823a43295ca3c60fa52cfe7
Shiffted <shifftc@gmail.com>
2026-03-08 20:24:58 +0100

    test: use xmake's pass_output_files

--- libs/oktest/xmake.lua
+++ libs/oktest/xmake.lua
@@ -28,7 +28,6 @@ add_requires("fmt >=12.0.0")
 ----------------------------------------------------------------------------------------------------
 --- Targets
 
-includes("tests/outputs/*.lua")
 target("oktest", function()
     set_kind(has_config("use_modules") and "moduleonly" or "static")
     add_deps("okutils", "okbitflag")
@@ -52,10 +51,10 @@ target("oktest", function()
     add_tests("example", {kind = "binary", files = "tests/examples.cpp", build_should_pass = is_cross()})
     add_tests("fail_compile_time_assert", { kind = "binary", files = "tests/fail_compile_time_assert.cpp", build_should_fail = true})
     add_tests("fail_compile_time_test", { kind = "binary", files = "tests/fail_compile_time_test.cpp", build_should_fail = true})
-    add_tests("macros", {kind = "binary", files = "tests/macros.cpp", plain = true, pass_outputs = macros_ouput, build_should_pass = is_cross()})
-    add_tests("short_macros", {kind = "binary", files = "tests/short_macros.cpp", plain = true, pass_outputs = macros_ouput, build_should_pass = is_cross()})
-    add_tests("sections", {kind = "binary", files = "tests/sections.cpp", plain = true, pass_outputs = sections_output, build_should_pass = is_cross()})
-    add_tests("success", {kind = "binary", files = "tests/success.cpp", plain = true, pass_outputs = success_ouput, build_should_pass = is_cross()})
+    add_tests("macros", {kind = "binary", files = "tests/macros.cpp", plain = true, pass_output_files = "tests/outputs/macros.txt", build_should_pass = is_cross()})
+    add_tests("short_macros", {kind = "binary", files = "tests/short_macros.cpp", plain = true, pass_output_files = "tests/outputs/macros.txt", build_should_pass = is_cross()})
+    add_tests("sections", {kind = "binary", files = "tests/sections.cpp", plain = true, pass_output_files = "tests/outputs/sections.txt", build_should_pass = is_cross()})
+    add_tests("success", {kind = "binary", files = "tests/success.cpp", plain = true, pass_output_files = "tests/outputs/success.txt", build_should_pass = is_cross()})
 
     add_tests("bench_asserts", {kind = "binary", group = "bench", files = "tests/benchmarks/asserts.cpp", build_should_pass = is_cross()})
     add_tests("bench_include", {kind = "binary", group = "bench", files = "tests/benchmarks/include.cpp", build_should_pass = is_cross()})
